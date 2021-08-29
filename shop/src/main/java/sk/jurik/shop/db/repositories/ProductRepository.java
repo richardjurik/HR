7c76940f43dcc81bb79315b0e0a354ac42d75435
risojurik <risojurik@gmail.com>
2021-08-29 15:57:09 +0200

    Create mapper, repository of product

---
+++ shop/src/main/java/sk/jurik/shop/db/repositories/ProductRepository.java
@@ -0,0 +1,81 @@
+package sk.jurik.shop.db.repositories;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+import sk.jurik.shop.db.mappers.CustomerRowMapper;
+import sk.jurik.shop.db.mappers.ProductRowMapper;
+import sk.jurik.shop.db.services.api.request.UpdateProductRequest;
+import sk.jurik.shop.domain.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.List;
+
+@Component
+public class ProductRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final ProductRowMapper productRowMapper = new ProductRowMapper();
+
+    public ProductRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Product getProduct(int id){
+        final String sql = "select * from product where product.id = " + id;
+
+        try {
+            return jdbcTemplate.queryForObject(sql,productRowMapper);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public Integer addProduct(Product product){
+        final String sql = "insert into product(merchant_id, name, description, price, created_at, available) values(?, ?, ?, ?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+                ps.setInt(1,product.getMerchantId());
+                ps.setString(2,product.getName());
+                ps.setString(3,product.getDescription());
+                ps.setDouble(4,product.getPrice());
+                ps.setTimestamp(5,product.getCreatedAt());
+                ps.setInt(6,product.getAvailable());
+
+                return ps;
+            }
+        },keyHolder);
+
+        if (keyHolder.getKey() != null) {
+            return keyHolder.getKey().intValue();
+        } else {
+            return null;
+        }
+    }
+
+    public List<Product> getAllProducts() {
+        final String sql = "select * from product";
+        return jdbcTemplate.query(sql, productRowMapper);
+    }
+
+    public void updateProduct(int id, UpdateProductRequest request){
+        final String sql = "update product set name = ?, description = ?, price = ?, available = ? where id = ?";
+
+        jdbcTemplate.update(sql, request.getName(),request.getDescription(),request.getPrice(),request.getAvailable(), id);
+    }
+
+    public void deleteProduct(int id) {
+        final String sql = "delete from product where id = ?";
+        jdbcTemplate.update(sql,id);
+    }
+}
---
+++ shop/src/main/java/sk/jurik/shop/db/services/impl/ProductServiceImpl.java
@@ -0,0 +1,44 @@
+package sk.jurik.shop.db.services.impl;
+
+import org.springframework.stereotype.Service;
+import sk.jurik.shop.db.repositories.ProductRepository;
+import sk.jurik.shop.db.services.api.ProductService;
+import sk.jurik.shop.db.services.api.request.UpdateProductRequest;
+import sk.jurik.shop.domain.Product;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.getAllProducts();
+    }
+
+    @Override
+    public Product getProduct(int id) {
+        return productRepository.getProduct(id);
+    }
+
+    @Override
+    public Integer addProduct(Product product) {
+        return productRepository.addProduct(product);
+    }
+
+    @Override
+    public void deleteProduct(int id) {
+        productRepository.deleteProduct(id);
+    }
+
+    @Override
+    public void updateProduct(int id, UpdateProductRequest request) {
+        productRepository.updateProduct(id, request);
+    }
+}
